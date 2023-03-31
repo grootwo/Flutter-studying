a5f886f6f128c1c6658114253908f311770c13b1
leegaeun <82134672+soaringwave@users.noreply.github.com>
2023-03-31 22:13:05 +0900

    Make final

--- dart/main.dart
+++ dart/main.dart
@@ -1,9 +1,5 @@
 void main() {
-  // name can be null or String
-  String? name = 'Hi';
-  name?.isEmpty;
-  // these are same
-//   if (name != null) {
-//     name.isEmpty;
-//   }
+  final num = 0;
+  // final can be set only once
+  // num = 1;
 }
