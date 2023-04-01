a46d870565f4352f41185e3a70f534c61f8b71f4
leegaeun <82134672+soaringwave@users.noreply.github.com>
2023-04-01 23:37:26 +0900

    Use string

--- dart/main.dart
+++ dart/main.dart
@@ -1,7 +1,6 @@
 void main() {
-  var check = true;
-  var numbers = [1, 2, 3, 4,
-  if(check) 5,
-  ];
-  print(numbers);
+  var name = 'Hello';
+  var number = 10;
+  var str = '$name World for ${number * 10} \'times\'';
+  print(name1);
 }
