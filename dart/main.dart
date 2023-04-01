54c97004da2d6bc01ef9e4ec08f898f242d40793
leegaeun <82134672+soaringwave@users.noreply.github.com>
2023-04-01 23:33:22 +0900

    Use collection if

--- dart/main.dart
+++ dart/main.dart
@@ -1,7 +1,7 @@
 void main() {
-  var numbers = [1, 2, 3, 4,];
-  numbers.add(5);
+  var check = true;
+  var numbers = [1, 2, 3, 4,
+  if(check) 5,
+  ];
   print(numbers);
-  print(numbers.first);
-  print(numbers.last);
 }
