a4838376e3eae416de4ce379693aadfcb7193958
leegaeun <82134672+soaringwave@users.noreply.github.com>
2023-04-04 21:54:31 +0900

    Use qq equals

--- dart/main.dart
+++ dart/main.dart
@@ -1,7 +1,6 @@
-String sayInfo(String name, String hobby, [int? num = 0]) {
-  return "$name likes to do $hobby for fun and input $num";
-}
-
 void main() {
-  print(sayInfo('Hola', 'Judo', 70));
+  String? text;
+  text ??= 'not null';
+  text ??= 'not null 2';
+  print(text);
 }
