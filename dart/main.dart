767a2bc2003864fa965c0489aefdc36be77dacea
leegaeun <82134672+soaringwave@users.noreply.github.com>
2023-04-04 12:02:25 +0900

    Make funcs and print

--- dart/main.dart
+++ dart/main.dart
@@ -1,8 +1,11 @@
+String sayName(String name) {
+  name = name + '!';
+  return 'It\'s $name';
+}
+
+num getSum(num num1, num num2) => num1 + num2;
+
 void main() {
-  List<int> numberList = [1, 1, 2, 2, 3, 3];
-  // [1, 1, 2, 2, 3, 3]
-  print(numberList);
-  Set<int> numberSet = {1, 1, 2, 2, 3, 3};
-  // {1, 2, 3}
-  print(numberSet);
+  print(sayName('hola'));
+  print(getSum(0, 10));
 }
