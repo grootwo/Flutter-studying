e6b791b0c35665de98151e3565ad4ee6c98e9bb4
leegaeun <82134672+soaringwave@users.noreply.github.com>
2023-04-06 23:14:43 +0900

    Make constructor shorter

--- dart/main.dart
+++ dart/main.dart
@@ -1,10 +1,7 @@
 class Player {
 	late final String name;
 	late int xp;
-	Player(String name, int xp) {
-		this.name = name;
-		this.xp = xp;
-	}
+	Player(this.name, this.xp);
 	void sayHello() {
 		print('hi my name is $name ($xp)');
 	}
