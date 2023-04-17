889d572b149dfbe566b80e7371fded7d752ad8c6
leegaeun <82134672+soaringwave@users.noreply.github.com>
2023-04-17 21:26:57 +0900

    Hello World

--- toonviewer/lib/main.dart
+++ toonviewer/lib/main.dart
@@ -7,6 +7,8 @@ void main() {
 class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp();
+    return MaterialApp(
+      home: Text("Hello World!"),
+    );
   }
 }
