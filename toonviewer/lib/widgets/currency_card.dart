9937c024b38d6b7e95c903e313e40a48a5b1ee05
leegaeun <82134672+soaringwave@users.noreply.github.com>
2023-04-30 15:21:16 +0900

    Make currency card class

---
+++ toonviewer/lib/widgets/currency_card.dart
@@ -0,0 +1,87 @@
+import 'package:flutter/material.dart';
+
+class Card extends StatelessWidget {
+  final String currency;
+  final String amount;
+  final String code;
+  final IconData icon;
+  final bool isInverted;
+
+  final Color _black = const Color(0xFF1F2123);
+
+  const Card({
+    super.key,
+    required this.currency,
+    required this.amount,
+    required this.code,
+    required this.icon,
+    required this.isInverted,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      clipBehavior: Clip.hardEdge,
+      decoration: BoxDecoration(
+        color: isInverted ? Colors.white : _black,
+        borderRadius: BorderRadius.circular(25),
+      ),
+      child: Padding(
+        padding: const EdgeInsets.all(35),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  currency,
+                  style: TextStyle(
+                    color: isInverted ? Colors.black : Colors.white,
+                    fontSize: 30,
+                  ),
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                Row(
+                  children: [
+                    Text(
+                      amount,
+                      style: TextStyle(
+                        color: isInverted ? Colors.black : Colors.white,
+                        fontSize: 20,
+                      ),
+                    ),
+                    const SizedBox(
+                      width: 10,
+                    ),
+                    Text(
+                      code,
+                      style: TextStyle(
+                        color: isInverted ? Colors.black : Colors.white,
+                        fontWeight: FontWeight.w300,
+                        fontSize: 20,
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+            Transform.scale(
+              scale: 2.3,
+              child: Transform.translate(
+                offset: const Offset(0, 15),
+                child: Icon(
+                  icon,
+                  color: isInverted ? Colors.black : Colors.white,
+                  size: 100,
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
