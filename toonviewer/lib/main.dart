66699bd0116e1f8f51997f49435b85a2f2d5fa20
leegaeun <82134672+soaringwave@users.noreply.github.com>
2023-05-06 17:30:20 +0900

    Make home screen and add into home

--- toonviewer/lib/main.dart
+++ toonviewer/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:toonviewer/screens/home_screen.dart';
 
 void main() {
   runApp(const App());
@@ -20,6 +21,7 @@ class App extends StatelessWidget {
         ),
         cardColor: const Color(0xFFF4EDDB),
       ),
+      home: const HomeScreen(),
     );
   }
 }
