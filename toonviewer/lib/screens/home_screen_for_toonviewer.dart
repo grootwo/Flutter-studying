ffe3550ea89763da6e634e35537d11e72544a863
leegaeun <82134672+soaringwave@users.noreply.github.com>
2023-05-08 17:31:03 +0900

    Set Appbar

--- toonviewer/lib/screens/home_screen_for_toonviewer.dart
+++ toonviewer/lib/screens/home_screen_for_toonviewer.dart
@@ -1,7 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/framework.dart';
-import 'package:flutter/src/widgets/placeholder.dart';
-import 'package:toonviewer/main.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -15,8 +12,15 @@ class _HomeScreenState extends State<HomeScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        foregroundColor: Colors.black,
+        backgroundColor: Colors.white,
+        elevation: 5,
+        toolbarHeight: 60.0,
         title: const Text(
           "Today's webtoon",
+          style: TextStyle(
+            fontSize: 23,
+          ),
         ),
       ),
     );
