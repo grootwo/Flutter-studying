5ea5f0e3ae49c5d86929e46e6da304efc78bdf21
leegaeun <82134672+soaringwave@users.noreply.github.com>
2023-05-08 23:00:44 +0900

    Test api service

--- toonviewer/lib/main.dart
+++ toonviewer/lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:toonviewer/API/api_service.dart';
 import 'package:toonviewer/screens/home_screen_for_toonviewer.dart';
 
 void main() {
+  ApiService().getTodayToons();
   runApp(const App());
 }
 
