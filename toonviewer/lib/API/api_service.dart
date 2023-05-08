e1f246488cebd084850d02ca40956036a52fcc4a
leegaeun <82134672+soaringwave@users.noreply.github.com>
2023-05-08 22:49:32 +0900

    Make Api service

---
+++ toonviewer/lib/API/api_service.dart
@@ -0,0 +1,16 @@
+import 'package:http/http.dart' as http;
+
+class ApiService {
+  String baseUri = "https://webtoon-crawler.nomadcoders.workers.dev";
+  String today = "today";
+
+  void getTodayToons() async {
+    final url = Uri.parse("$baseUri/$today");
+    final response = await http.get(url);
+    if (response.statusCode == 200) {
+      print(response);
+      return;
+    }
+    throw Error();
+  }
+}
