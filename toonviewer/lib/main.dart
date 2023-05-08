b7f71e854ee47ff017bc52240c3039917282db38
leegaeun <82134672+soaringwave@users.noreply.github.com>
2023-05-08 17:27:41 +0900

    Make toonviewer file

--- toonviewer/lib/main.dart
+++ toonviewer/lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:toonviewer/screens/home_screen_for_pomodoro.dart';
+import 'package:toonviewer/screens/home_screen_for_toonviewer.dart';
 
 void main() {
   runApp(const App());
@@ -10,18 +10,8 @@ class App extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      theme: ThemeData(
-        colorScheme:
-            ColorScheme.fromSwatch(backgroundColor: const Color(0xFFE7626C)),
-        textTheme: const TextTheme(
-          displayLarge: TextStyle(
-            color: Color(0xFF232B55),
-          ),
-        ),
-        cardColor: const Color(0xFFF4EDDB),
-      ),
-      home: const HomeScreen(),
+    return const MaterialApp(
+      home: HomeScreen(),
     );
   }
 }
---
+++ toonviewer/lib/screens/home_screen_for_toonviewer.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:toonviewer/main.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          "Today's webtoon",
+        ),
+      ),
+    );
+  }
+}
