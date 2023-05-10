1d78ff6f71580db36dbfc7f9a638a123d8ef66f1
leegaeun <82134672+soaringwave@users.noreply.github.com>
2023-05-10 10:17:40 +0900

    Set code returning WebtoonModel list

--- toonviewer/lib/API/api_service.dart
+++ toonviewer/lib/API/api_service.dart
@@ -1,15 +1,22 @@
+import 'dart:convert';
+
 import 'package:http/http.dart' as http;
+import 'package:toonviewer/models/webtoon_model.dart';
 
 class ApiService {
   String baseUri = "https://webtoon-crawler.nomadcoders.workers.dev";
   String today = "today";
 
-  void getTodayToons() async {
+  Future<List<WebtoonModel>> getTodayToons() async {
+    List<WebtoonModel> webtoons = [];
     final url = Uri.parse("$baseUri/$today");
     final response = await http.get(url);
     if (response.statusCode == 200) {
-      print(response.body);
-      return;
+      final List<dynamic> webtoonCodes = jsonDecode(response.body);
+      for (var webtoon in webtoonCodes) {
+        webtoons.add(WebtoonModel.fromJson(webtoon));
+      }
+      return webtoons;
     }
     throw Error();
   }
