a354e9519aa21e908471856286b7d22585dbf19e
leegaeun <82134672+soaringwave@users.noreply.github.com>
2023-05-12 09:30:29 +0900

    Add titles

--- movieviewer/lib/screens/home_scree.dart
+++ movieviewer/lib/screens/home_scree.dart
@@ -7,15 +7,48 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      body: Column(
-        children: const [
-          Text(
-            'Popular Movie',
-            style: TextStyle(
-              fontSize: 30,
+      body: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Column(
+          children: [
+            const SizedBox(
+              height: 100,
             ),
-          ),
-        ],
+            Row(
+              children: const [
+                Text(
+                  'Popular Movies',
+                  style: TextStyle(
+                    fontSize: 30,
+                    fontWeight: FontWeight.w700,
+                  ),
+                ),
+              ],
+            ),
+            Row(
+              children: const [
+                Text(
+                  'Now in Cinemas',
+                  style: TextStyle(
+                    fontSize: 30,
+                    fontWeight: FontWeight.w700,
+                  ),
+                ),
+              ],
+            ),
+            Row(
+              children: const [
+                Text(
+                  'Coming Soon',
+                  style: TextStyle(
+                    fontSize: 30,
+                    fontWeight: FontWeight.w700,
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
