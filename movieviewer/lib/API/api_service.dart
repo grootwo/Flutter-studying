060866eaef6153f9f39dc688c1e0de5ea1688921
leegaeun <82134672+soaringwave@users.noreply.github.com>
2023-05-14 23:16:19 +0900

    Make movie detail api service

--- movieviewer/lib/API/api_service.dart
+++ movieviewer/lib/API/api_service.dart
@@ -3,6 +3,8 @@ import 'dart:convert';
 import 'package:http/http.dart' as http;
 import 'package:movieviewer/models/movie_model.dart';
 
+import '../models/movie_detail_model.dart';
+
 class ApiService {
   final baseUrl = 'https://movies-api.nomadcoders.workers.dev';
 
@@ -13,11 +15,21 @@ class ApiService {
     if (response.statusCode == 200) {
       final Map<String, dynamic> jsonCode = jsonDecode(response.body);
       final List<dynamic> jsonCodeResults = jsonCode["results"];
-      print(jsonCodeResults);
       for (var movie in jsonCodeResults) {
         movies.add(MovieModel.fromJson(movie));
       }
       return movies;
+    }
+    throw Error();
+  }
+
+  Future<MovieDetailModel> getMovieById(int id) async {
+    final url =
+        Uri.parse("https://movies-api.nomadcoders.workers.dev/movie?id=${id}");
+    final response = await http.get(url);
+    if (response.statusCode == 200) {
+      final movie = jsonDecode(response.body);
+      return MovieDetailModel.fromJson(movie);
     }
     throw Error();
   }
