68ac6f8653721b10531cdb3884d0ee0e605ad74a
soaringwave <82134672+soaringwave@users.noreply.github.com>
2023-09-26 03:02:56 +0900

    Make variables

--- toonlinkers/lib/conception.dart
+++ toonlinkers/lib/conception.dart
@@ -1,4 +1,11 @@
 // semi colon and main function are important
 void main() {
   print("hello world!");
+
+  // inside of function and method, use var
+  var animal = 'a cat';
+  animal = 'a dog';
+  // inside of class, use specific type
+  String name = 'a cat';
+  name = 'a dog'; 
 }
