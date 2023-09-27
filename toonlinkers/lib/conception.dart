6fa0b50cb31e9ba1748cdf97654f16da992ccb96
soaringwave <82134672+soaringwave@users.noreply.github.com>
2023-09-27 17:55:32 +0900

    Delete former code and make some functions

--- toonlinkers/lib/conception.dart
+++ toonlinkers/lib/conception.dart
@@ -1,107 +1,12 @@
-// semi colon and main function are important
+void sayHallo(String name) {
+  print('Hallo $name');
+}
+
+String returnHallo(String name) {
+  return 'Hallo $name';
+}
+
 void main() {
-  print("hello world!");
-
-  // inside of function and method, use var
-  var animal = 'a cat';
-  animal = 'a dog';
-  // inside of class, use specific type
-  String name = 'a cat';
-  name = 'a dog';
-
-  // something is dynamic type
-  var something;
-  something = 100;
-  something = 'one hundred';
-  something = true;
-  if (something is String) {
-    print(something.length);
-  }
-  if (something is int) {
-    print(something.isOdd);
-  }
-
-  // nullable can be possible with ?
-  String greeting = 'hello';
-  // greeting = null;
-  String? nullGreeting = 'hello';
-  nullGreeting = null;
-  // if nullGreeting is not null, do next
-  nullGreeting?.isEmpty;
-
-  // final var won't be changed
-  final lastVar = 'This is final definition';
-
-  // late var can be assigned later, but can be used after assignment
-  late final String lately;
-  // do something, go to api
-  lately = 'hallo';
-  // lately = 'hallo later';
-
-  // constant is known while compile time
-  const anExample = 'http://something';
-
-  // list
-  var check = true;
-  var nums = [
-    0,
-    1,
-    2,
-    3,
-    4,
-    if (check) 5,
-  ];
-  List<String> strs = [
-    'abc',
-    'bcd',
-    'cde',
-  ];
-  print(nums.last);
-  print(strs.isNotEmpty);
-
-  // string interpolation
-  var meaning = '안녕';
-  var count = 0;
-  var sentence = '\'hallo means $meaning, and you can say ${count + 1}\'';
-  print(sentence);
-
-  // collection for
-  var cuteAnimals = [
-    'cat',
-    'dog',
-  ];
-  var coolAnimals = [
-    'koala',
-    'kangaroo',
-    'squirrel',
-    for (var animal in cuteAnimals) '$animal 🥳',
-  ];
-
-  // map
-  var gardener = {
-    'name': 'Hoho',
-    'hp': 100,
-    'experience': 'a lot',
-  };
-  Map<List<int>, String> numbers = {
-    [1]: 'one',
-    [2, 2]: 'two',
-  };
-  print(numbers.keys);
-  List<Map<int, int>> intList = [
-    {
-      1: 1,
-      2: 2,
-    },
-    {
-      3: 3,
-      4: 4,
-    }
-  ];
-
-  // all items in sets are unique
-  Set<int> uniqueNums = {1, 2, 3, 4};
-  uniqueNums.add(1);
-  uniqueNums.add(1);
-  print(uniqueNums);
+  sayHallo('some cat');
+  print(returnHallo('some dog'));
 }
