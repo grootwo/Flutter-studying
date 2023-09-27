648e78a8159a77076d41c62562d96f3dcf4e2790
soaringwave <82134672+soaringwave@users.noreply.github.com>
2023-09-27 18:25:53 +0900

    Use qq operator

--- toonlinkers/lib/conception.dart
+++ toonlinkers/lib/conception.dart
@@ -1,6 +1,10 @@
-String makeUpper(String? str) => str != null ? str.toUpperCase() : 'UNKNOWN';
+String makeUpper(String? str) => str?.toUpperCase() ?? 'UNKNWON';
 
 void main() {
   makeUpper('hallo');
   makeUpper(null);
+  String? check;
+  // if check is null, put following value
+  check ??= 'nullable';
+  check ??= 'another value';
 }
