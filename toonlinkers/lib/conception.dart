eb08583e2c42a0bc082c5ed99891ae73e7ac1c76
soaringwave <82134672+soaringwave@users.noreply.github.com>
2023-09-27 17:57:12 +0900

    Add fat arrow function

--- toonlinkers/lib/conception.dart
+++ toonlinkers/lib/conception.dart
@@ -6,7 +6,10 @@ String returnHallo(String name) {
   return 'Hallo $name';
 }
 
+num mutiple(num num1, num num2) => num1 * num2;
+
 void main() {
   sayHallo('some cat');
   print(returnHallo('some dog'));
+  var answer = mutiple(3, 10.3);
 }
