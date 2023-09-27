bcf60c07f25d6c50923030b410af561a13317c5d
soaringwave <82134672+soaringwave@users.noreply.github.com>
2023-09-27 18:03:11 +0900

    Make function with named arguments

--- toonlinkers/lib/conception.dart
+++ toonlinkers/lib/conception.dart
@@ -1,15 +1,15 @@
-void sayHallo(String name) {
-  print('Hallo $name');
+String makeIntro({
+  String name = 'anon',
+  int age = 0,
+  String major = 'unknown',
+}) {
+  return 'name: $name, age: $age, major: $major';
 }
-
-String returnHallo(String name) {
-  return 'Hallo $name';
-}
-
-num mutiple(num num1, num num2) => num1 * num2;
 
 void main() {
-  sayHallo('some cat');
-  print(returnHallo('some dog'));
-  var answer = mutiple(3, 10.3);
+  print(makeIntro(
+    name: 'hola',
+    major: 'computer science',
+    age: 30,
+  ));
 }
