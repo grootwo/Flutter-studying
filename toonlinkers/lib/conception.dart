3f5cfdca97cc0a3cecb1490f9e151c745c6efdf3
soaringwave <82134672+soaringwave@users.noreply.github.com>
2023-09-27 18:16:07 +0900

    Make optional positional parameter

--- toonlinkers/lib/conception.dart
+++ toonlinkers/lib/conception.dart
@@ -1,16 +1,6 @@
-String makeIntro({
-  required String name,
-  required int age,
-  required String major,
-}) {
-  return 'name: $name, age: $age, major: $major';
-}
+String makeIntro(String name, int age, [String? major = 'unknown']) =>
+    'name: $name, age: $age, major: $major';
 
 void main() {
-  print(makeIntro(
-    name: 'hola',
-    major: 'computer science',
-    age: 30,
-  ));
-  // makeIntro();
+  print(makeIntro('hola', 30));
 }
