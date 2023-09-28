db8ee1eabf23cefb195a21f5ffe1773fa262fedb
soaringwave <82134672+soaringwave@users.noreply.github.com>
2023-09-28 18:39:11 +0900

    Make constructor with named parameters

--- toonlinkers/lib/conception.dart
+++ toonlinkers/lib/conception.dart
@@ -1,21 +1,30 @@
 class Cat {
-  late String color;
-  late final String state;
+  String color;
+  final String state;
+  int age;
 
-  // Cat(String color, String state) {
-  //   this.color = color;
-  //   this.state = state;
-  // }
-  Cat(this.color, this.state);
+  Cat({
+    required this.color,
+    required this.state,
+    required this.age,
+  });
 
   void meow() {
-    print('meow $color cat! I\'m $state');
+    print('meow $color cat! I\'m $state and $age');
   }
 }
 
 void main() {
-  Cat cat1 = Cat('cheese', 'healthy');
-  Cat cat2 = Cat('black and white', 'healthy');
+  Cat cat1 = Cat(
+    color: 'cheese',
+    state: 'healthy',
+    age: 15,
+  );
+  Cat cat2 = Cat(
+    color: 'black and white',
+    state: 'healthy',
+    age: 6,
+  );
   cat1.meow();
   cat2.meow();
 }
