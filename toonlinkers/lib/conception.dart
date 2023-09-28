6db5b2e54dc533a5290d345425fe0ff4e8975323
soaringwave <82134672+soaringwave@users.noreply.github.com>
2023-09-28 18:49:46 +0900

    Make named constructors

--- toonlinkers/lib/conception.dart
+++ toonlinkers/lib/conception.dart
@@ -1,6 +1,5 @@
 class Cat {
-  String color;
-  final String state;
+  String color, state;
   int age;
 
   Cat({
@@ -9,22 +8,40 @@ class Cat {
     required this.age,
   });
 
+  Cat.createCheese({
+    required String state,
+    required int age,
+  })  : this.state = state,
+        this.age = age,
+        this.color = 'cheese';
+
+  Cat.createTabby({
+    required String state,
+    required int age,
+  })  : this.state = state,
+        this.age = age,
+        this.color = 'tabby';
+
   void meow() {
     print('meow $color cat! I\'m $state and $age');
   }
 }
 
 void main() {
-  Cat cat1 = Cat(
-    color: 'cheese',
+  Cat cat0 = Cat(
+    color: 'gray',
     state: 'healthy',
     age: 15,
   );
-  Cat cat2 = Cat(
-    color: 'black and white',
+  Cat cat1 = Cat.createCheese(
+    state: 'healthy',
+    age: 15,
+  );
+  Cat cat2 = Cat.createTabby(
     state: 'healthy',
     age: 6,
   );
+  cat0.meow();
   cat1.meow();
   cat2.meow();
 }
