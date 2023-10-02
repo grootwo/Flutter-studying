6ebdfc9b5137b69133bebb289becbbdea5b84b60
soaringwave <82134672+soaringwave@users.noreply.github.com>
2023-10-02 22:44:10 +0900

    Start new Stateful widget

--- toonlinkers/lib/main.dart
+++ toonlinkers/lib/main.dart
@@ -1,149 +1,22 @@
 import 'package:flutter/material.dart';
-import 'package:toonlinkers/widgets/Button.dart';
-import 'package:toonlinkers/widgets/CurrenyCard.dart';
 
 void main() {
   runApp(const App());
 }
 
-class App extends StatelessWidget {
+class App extends StatefulWidget {
   const App({super.key});
 
   @override
+  State<App> createState() => _AppState();
+}
+
+class _AppState extends State<App> {
+  @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       home: Scaffold(
-        backgroundColor: const Color(0xFF181818),
-        body: Padding(
-          padding: const EdgeInsets.symmetric(
-            horizontal: 25,
-          ),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              const SizedBox(
-                height: 50,
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.end,
-                children: [
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.end,
-                    children: [
-                      const Text(
-                        'Hey, Hwido',
-                        style: TextStyle(
-                          fontSize: 35,
-                          color: Colors.white,
-                        ),
-                      ),
-                      const SizedBox(
-                        height: 10,
-                      ),
-                      Text(
-                        'Welcome back',
-                        style: TextStyle(
-                          fontSize: 20,
-                          color: Colors.white.withOpacity(0.8),
-                        ),
-                      ),
-                    ],
-                  )
-                ],
-              ),
-              const SizedBox(
-                height: 50,
-              ),
-              const Text(
-                'Total Balance',
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 20,
-                ),
-              ),
-              const SizedBox(
-                height: 10,
-              ),
-              const Text(
-                '\$5 194 382',
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 40,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-              const SizedBox(
-                height: 20,
-              ),
-              const Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Button(
-                    text: 'Transfer',
-                    bgColor: Colors.amber,
-                    textColor: Colors.black,
-                  ),
-                  Button(
-                    text: 'Request',
-                    bgColor: Color.fromARGB(255, 69, 69, 69),
-                    textColor: Colors.white,
-                  ),
-                ],
-              ),
-              const SizedBox(
-                height: 50,
-              ),
-              const Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  Text(
-                    'Wallets',
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 30,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                  Text(
-                    'View All',
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 20,
-                    ),
-                  ),
-                ],
-              ),
-              const SizedBox(
-                height: 20,
-              ),
-              const CurrencyCard(
-                currencyType: 'Dollar',
-                price: '55 622',
-                currencyTypeShort: 'USD',
-                currencyIcon: Icons.currency_exchange_rounded,
-                isInverted: true,
-                dy: 0,
-              ),
-              const CurrencyCard(
-                currencyType: 'Euro',
-                price: '6 428',
-                currencyTypeShort: 'EUR',
-                currencyIcon: Icons.euro_rounded,
-                isInverted: false,
-                dy: -2,
-              ),
-              const CurrencyCard(
-                currencyType: 'Bitcoin',
-                price: '28 981',
-                currencyTypeShort: 'BTC',
-                currencyIcon: Icons.currency_bitcoin_rounded,
-                isInverted: true,
-                dy: -4,
-              ),
-            ],
-          ),
-        ),
+        backgroundColor: Colors.blueGrey,
       ),
     );
   }
