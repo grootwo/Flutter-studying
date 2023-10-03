f367ab5520331c1bca3991a1c634b76a730dce2d
soaringwave <82134672+soaringwave@users.noreply.github.com>
2023-10-03 12:03:40 +0200

    Set titleLarge theme

--- toonlinkers/lib/main.dart
+++ toonlinkers/lib/main.dart
@@ -12,50 +12,44 @@ class App extends StatefulWidget {
 }
 
 class _AppState extends State<App> {
-  List<int> nums = [];
-  int count = 0;
-
-  void onClicked() {
-    setState(() {
-      count = count + 1;
-      nums.add(count);
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
+      theme: ThemeData(
+        textTheme: const TextTheme(
+          titleLarge: TextStyle(
+            color: Colors.greenAccent,
+            fontSize: 60,
+          ),
+        ),
+      ),
+      home: const Scaffold(
         backgroundColor: Colors.blueGrey,
         body: Center(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              const Text(
-                'Click Count',
-                style: TextStyle(
-                  fontSize: 50,
-                  color: Colors.white,
-                ),
-              ),
-              for (var num in nums)
-                Text(
-                  '$num',
-                  style: const TextStyle(
-                    fontSize: 50,
-                    color: Colors.white,
-                  ),
-                ),
-              IconButton(
-                iconSize: 70,
-                onPressed: onClicked,
-                icon: const Icon(
-                  Icons.add_box,
-                ),
-              ),
+              MyLargeTitle(),
             ],
           ),
         ),
+      ),
+    );
+  }
+}
+
+class MyLargeTitle extends StatelessWidget {
+  const MyLargeTitle({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      'My Large Title',
+      style: TextStyle(
+        fontSize: Theme.of(context).textTheme.titleLarge?.fontSize,
+        color: Theme.of(context).textTheme.titleLarge?.color,
       ),
     );
   }
