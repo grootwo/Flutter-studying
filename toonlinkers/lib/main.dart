67a8d97849f9344a7172995104baf92e969c3662
soaringwave <82134672+soaringwave@users.noreply.github.com>
2023-10-03 14:58:03 +0530

    Make click counter and button

--- toonlinkers/lib/main.dart
+++ toonlinkers/lib/main.dart
@@ -12,11 +12,45 @@ class App extends StatefulWidget {
 }
 
 class _AppState extends State<App> {
+  int counter = 0;
+
+  void onClicked() {
+    counter = counter + 1;
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.blueGrey,
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Text(
+                'Click Count',
+                style: TextStyle(
+                  fontSize: 50,
+                  color: Colors.white,
+                ),
+              ),
+              Text(
+                '$counter',
+                style: const TextStyle(
+                  fontSize: 50,
+                  color: Colors.white,
+                ),
+              ),
+              IconButton(
+                iconSize: 70,
+                onPressed: onClicked,
+                icon: const Icon(
+                  Icons.add_box,
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
