1140f5665d37f8759480f976b64475522f550bc9
soaringwave <82134672+soaringwave@users.noreply.github.com>
2023-10-04 08:45:45 +0200

    Set pomodoro theme

--- toonlinkers/lib/main.dart
+++ toonlinkers/lib/main.dart
@@ -12,77 +12,21 @@ class App extends StatefulWidget {
 }
 
 class _AppState extends State<App> {
-  bool check = true;
-
-  void toggleText() {
-    setState(() {
-      check = !check;
-    });
-  }
-
-  @override
-  void initState() {
-    // TODO: implement initState
-    super.initState();
-    print('initState');
-  }
-
-  @override
-  void dispose() {
-    // TODO: implement dispose
-    super.dispose();
-    print('dispose');
-  }
-
   @override
   Widget build(BuildContext context) {
-    print('build');
     return MaterialApp(
       theme: ThemeData(
+        colorScheme:
+            ColorScheme.fromSwatch(backgroundColor: const Color(0xFFE7626C)),
         textTheme: const TextTheme(
-          titleLarge: TextStyle(
-            color: Colors.greenAccent,
-            fontSize: 60,
+          displayLarge: TextStyle(
+            color: Color(0xFF232B55),
           ),
         ),
+        cardColor: const Color(0xFFF4EDDB),
       ),
-      home: Scaffold(
-        backgroundColor: Colors.blueGrey,
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              check
-                  ? const MyLargeTitle()
-                  : const Text(
-                      'Bye',
-                      style: TextStyle(
-                        fontSize: 60,
-                      ),
-                    ),
-              IconButton(
-                  onPressed: toggleText,
-                  icon: const Icon(Icons.ads_click_rounded)),
-            ],
-          ),
-        ),
-      ),
-    );
-  }
-}
-
-class MyLargeTitle extends StatelessWidget {
-  const MyLargeTitle({
-    super.key,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    return Text(
-      'My Large Title',
-      style: TextStyle(
-        fontSize: Theme.of(context).textTheme.titleLarge?.fontSize,
-        color: Theme.of(context).textTheme.titleLarge?.color,
+      home: const Scaffold(
+        body: Center(),
       ),
     );
   }
