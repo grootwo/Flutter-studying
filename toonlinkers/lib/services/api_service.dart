4a79da95ad441c7f32f840c5af7813f32a8998a5
soaringwave <82134672+soaringwave@users.noreply.github.com>
2023-10-12 11:30:13 +0200

    Change data type of Url into static const

--- toonlinkers/lib/services/api_service.dart
+++ toonlinkers/lib/services/api_service.dart
@@ -4,10 +4,11 @@ import 'package:http/http.dart' as http;
 import 'package:toonlinkers/models/webtoon_model.dart';
 
 class ApiService {
-  final String baseUrl = 'https://webtoon-crawler.nomadcoders.workers.dev';
-  final String today = 'today';
+  static const String baseUrl =
+      'https://webtoon-crawler.nomadcoders.workers.dev';
+  static const String today = 'today';
 
-  Future<List<WebtoonModel>> getTodayWebtoons() async {
+  static Future<List<WebtoonModel>> getTodayWebtoons() async {
     List<WebtoonModel> webtoons = [];
     final url = Uri.parse('$baseUrl/$today');
     final response = await http.get(url);
