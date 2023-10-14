e4e4c258ceb6bef9681b5467fe3873007a440f12
soaringwave <82134672+soaringwave@users.noreply.github.com>
2023-10-14 11:33:38 +0200

    Detect click and move to detail_screen

---
+++ toonlinkers/lib/screens/detail_screen.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+class DetailScreen extends StatelessWidget {
+  final String title, thumb, id;
+  const DetailScreen({
+    super.key,
+    required this.title,
+    required this.thumb,
+    required this.id,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        title: Text(
+          title,
+          style: const TextStyle(
+            fontSize: 25,
+            color: Colors.black,
+          ),
+        ),
+      ),
+    );
+  }
+}
--- toonlinkers/lib/widgets/Webtoon.dart
+++ toonlinkers/lib/widgets/Webtoon.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:toonlinkers/screens/detail_screen.dart';
 
 class Webtoon extends StatelessWidget {
   final String title, thumb, id;
@@ -12,36 +13,50 @@ class Webtoon extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Container(
-          clipBehavior: Clip.hardEdge,
-          width: 260,
-          decoration: BoxDecoration(
-            borderRadius: const BorderRadius.all(Radius.circular(15)),
-            boxShadow: [
-              BoxShadow(
-                color: Colors.black.withOpacity(0.3),
-                offset: const Offset(5, 5),
-                blurRadius: 5,
-              ),
-            ],
+    return GestureDetector(
+      onTap: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) => DetailScreen(
+              title: title,
+              thumb: thumb,
+              id: id,
+            ),
           ),
-          child: Image.network(
-            thumb,
+        );
+      },
+      child: Column(
+        children: [
+          Container(
+            clipBehavior: Clip.hardEdge,
+            width: 260,
+            decoration: BoxDecoration(
+              borderRadius: const BorderRadius.all(Radius.circular(15)),
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.black.withOpacity(0.3),
+                  offset: const Offset(5, 5),
+                  blurRadius: 5,
+                ),
+              ],
+            ),
+            child: Image.network(
+              thumb,
+            ),
           ),
-        ),
-        const SizedBox(
-          height: 10,
-        ),
-        Text(
-          title,
-          style: const TextStyle(
-            fontSize: 20,
-            fontWeight: FontWeight.w400,
+          const SizedBox(
+            height: 10,
           ),
-        ),
-      ],
+          Text(
+            title,
+            style: const TextStyle(
+              fontSize: 20,
+              fontWeight: FontWeight.w400,
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
