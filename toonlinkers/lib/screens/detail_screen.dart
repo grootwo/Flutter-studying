b27efa5053bfcf8ff4a39a035c90165b08802e38
soaringwave <82134672+soaringwave@users.noreply.github.com>
2023-10-18 14:57:26 +0200

    Fix wrong attachment

--- toonlinkers/lib/screens/detail_screen.dart
+++ toonlinkers/lib/screens/detail_screen.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:toonlinkers/models/Webtoon_detail_model.dart';
+import 'package:toonlinkers/models/webtoon_detail_model.dart';
 import 'package:toonlinkers/models/webtoon_episode_model.dart';
 import 'package:toonlinkers/services/api_service.dart';
 
