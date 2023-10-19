8190d143fcdb4994931ba5a245a7528aee371e16
soaringwave <82134672+soaringwave@users.noreply.github.com>
2023-10-19 12:01:41 +0200

    Delete constructor of webtoonEpisodeModel

--- toonlinkers/lib/models/webtoon_episode_model.dart
+++ toonlinkers/lib/models/webtoon_episode_model.dart
@@ -1,13 +1,6 @@
 class WebtoonEpisodeModel {
   final String title, id, rating, date;
 
-  WebtoonEpisodeModel(
-    this.title,
-    this.id,
-    this.rating,
-    this.date,
-  );
-
   WebtoonEpisodeModel.fromJson(Map<String, dynamic> json)
       : title = json['title'],
         id = json['id'],
