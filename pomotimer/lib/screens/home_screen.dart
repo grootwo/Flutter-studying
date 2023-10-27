114ff24591d681ed819ba6e8cacc1f764330eba1
soaringwave <82134672+soaringwave@users.noreply.github.com>
2023-10-27 22:13:54 +0200

    Change to stateful widget

--- pomotimer/lib/screens/home_screen.dart
+++ pomotimer/lib/screens/home_screen.dart
@@ -1,7 +1,14 @@
 import 'package:flutter/material.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  bool isWorking = true;
 
   @override
   Widget build(BuildContext context) {
