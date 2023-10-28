13dfce35f7ae33a9a184a0b95cd4198b7277a283
soaringwave <82134672+soaringwave@users.noreply.github.com>
2023-10-28 16:37:55 +0200

    Make timer's circle

--- pomotimer/lib/screens/home_screen.dart
+++ pomotimer/lib/screens/home_screen.dart
@@ -16,6 +16,7 @@ class _HomeScreenState extends State<HomeScreen> {
       body: Padding(
         padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 50),
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             const SizedBox(
               height: 50,
@@ -26,6 +27,39 @@ class _HomeScreenState extends State<HomeScreen> {
                 fontSize: 45,
               ),
             ),
+            const SizedBox(
+              height: 50,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Container(
+                  alignment: Alignment.center,
+                  height: 300,
+                  width: 300,
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(150),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.black.withOpacity(0.3),
+                        offset: const Offset(
+                          0,
+                          5,
+                        ),
+                        blurRadius: 10,
+                      ),
+                    ],
+                  ),
+                  child: const Text(
+                    '25:00',
+                    style: TextStyle(
+                      fontSize: 40,
+                    ),
+                  ),
+                ),
+              ],
+            )
           ],
         ),
       ),
