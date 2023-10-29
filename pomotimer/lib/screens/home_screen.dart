1f6a0608c68cdc19764ae98c7ea46b346c9dafda
soaringwave <82134672+soaringwave@users.noreply.github.com>
2023-10-29 11:59:24 +0100

    Change and set home screen design

--- pomotimer/lib/screens/home_screen.dart
+++ pomotimer/lib/screens/home_screen.dart
@@ -13,115 +13,148 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 50),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            const SizedBox(
-              height: 50,
-            ),
-            Text(
-              isWorking ? 'Working' : 'Resting',
-              style: const TextStyle(
-                fontSize: 45,
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Flexible(
+            flex: 1,
+            child: Container(
+              alignment: Alignment.bottomCenter,
+              child: Text(
+                isWorking ? 'Working' : 'Resting',
+                style: const TextStyle(
+                  fontSize: 45,
+                ),
               ),
             ),
-            const SizedBox(
-              height: 120,
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Container(
-                  alignment: Alignment.center,
-                  height: 300,
-                  width: 300,
-                  decoration: BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: BorderRadius.circular(150),
-                    boxShadow: [
-                      BoxShadow(
-                        color: Colors.black.withOpacity(0.3),
-                        offset: const Offset(
-                          0,
-                          5,
+          ),
+          Flexible(
+            flex: 3,
+            child: Container(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Container(
+                        alignment: Alignment.center,
+                        height: 300,
+                        width: 300,
+                        decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.circular(150),
+                          boxShadow: [
+                            BoxShadow(
+                              color: Colors.black.withOpacity(0.3),
+                              offset: const Offset(
+                                0,
+                                5,
+                              ),
+                              blurRadius: 10,
+                            ),
+                          ],
                         ),
-                        blurRadius: 10,
+                        child: const Text(
+                          '25:00',
+                          style: TextStyle(
+                            fontSize: 40,
+                          ),
+                        ),
                       ),
                     ],
                   ),
-                  child: const Text(
-                    '25:00',
-                    style: TextStyle(
-                      fontSize: 40,
-                    ),
+                  const SizedBox(
+                    height: 30,
                   ),
-                ),
-              ],
-            ),
-            const SizedBox(
-              height: 50,
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                Container(
-                  alignment: Alignment.center,
-                  height: 50,
-                  width: 50,
-                  decoration: BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: BorderRadius.circular(150),
-                    boxShadow: [
-                      BoxShadow(
-                        color: Colors.black.withOpacity(0.3),
-                        offset: const Offset(
-                          0,
-                          5,
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      Container(
+                        alignment: Alignment.center,
+                        height: 50,
+                        width: 50,
+                        decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.circular(150),
+                          boxShadow: [
+                            BoxShadow(
+                              color: Colors.black.withOpacity(0.3),
+                              offset: const Offset(
+                                0,
+                                5,
+                              ),
+                              blurRadius: 10,
+                            ),
+                          ],
                         ),
-                        blurRadius: 10,
+                        child: IconButton(
+                          onPressed: () {},
+                          icon: const Icon(
+                            Icons.play_arrow_rounded,
+                            size: 30,
+                          ),
+                        ),
+                      ),
+                      Container(
+                        alignment: Alignment.center,
+                        height: 50,
+                        width: 50,
+                        decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.circular(150),
+                          boxShadow: [
+                            BoxShadow(
+                              color: Colors.black.withOpacity(0.3),
+                              offset: const Offset(
+                                0,
+                                5,
+                              ),
+                              blurRadius: 10,
+                            ),
+                          ],
+                        ),
+                        child: IconButton(
+                          onPressed: () {},
+                          icon: const Icon(
+                            Icons.square_rounded,
+                            size: 20,
+                          ),
+                        ),
                       ),
                     ],
-                  ),
-                  child: IconButton(
-                    onPressed: () {},
-                    icon: const Icon(
-                      Icons.play_arrow_rounded,
-                      size: 50,
-                    ),
-                  ),
-                ),
-                Container(
-                  alignment: Alignment.center,
-                  height: 50,
-                  width: 50,
-                  decoration: BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: BorderRadius.circular(150),
-                    boxShadow: [
-                      BoxShadow(
-                        color: Colors.black.withOpacity(0.3),
-                        offset: const Offset(
-                          0,
-                          5,
-                        ),
-                        blurRadius: 10,
-                      ),
-                    ],
-                  ),
-                  child: IconButton(
-                    onPressed: () {},
-                    icon: const Icon(
-                      Icons.square_rounded,
-                      size: 30,
-                    ),
+                  )
+                ],
+              ),
+            ),
+          ),
+          Flexible(
+            flex: 1,
+            child: Container(
+              alignment: Alignment.topCenter,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(30),
+                color: Colors.white,
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.black.withOpacity(0.3),
+                    offset: const Offset(0, -1),
+                    blurRadius: 10,
+                  )
+                ],
+              ),
+              child: const Padding(
+                padding: EdgeInsetsDirectional.only(top: 30),
+                child: Text(
+                  "Total Time",
+                  style: TextStyle(
+                    fontSize: 40,
                   ),
                 ),
-              ],
-            )
-          ],
-        ),
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
