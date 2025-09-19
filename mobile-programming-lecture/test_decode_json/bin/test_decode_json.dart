6266d37ec643cfd8d195f59dec2d9488ac1e2b70
soaringwave <82134672+grootwo@users.noreply.github.com>
2025-09-19 09:55:20 +0900

    feat: check parsing

--- mobile-programming-lecture/test_decode_json/bin/test_decode_json.dart
+++ mobile-programming-lecture/test_decode_json/bin/test_decode_json.dart
@@ -12,4 +12,9 @@ void main() {
 
   print(jsonText == "[{'score': 40},{'score': 100},{'score': 120, 'overtime': true, 'special_guest': null}]"); // false
   print(jsonText == '[{"score":40},{"score":100},{"score":120,"overtime":true,"special_guest":null}]'); // true
+
+  final obj = jsonDecode(jsonText);
+  print(obj is List); // true
+  print(obj[2]['overtime'] == true); // true
+  print(obj[2]['special_guest'] == null); // true
 }
