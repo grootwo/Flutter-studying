9b36f55033f7498675ad884146b913be2c53c05e
soaringwave <82134672+grootwo@users.noreply.github.com>
2025-09-19 09:52:49 +0900

    feat: compare jsonText

--- mobile-programming-lecture/test_decode_json/bin/test_decode_json.dart
+++ mobile-programming-lecture/test_decode_json/bin/test_decode_json.dart
@@ -1,18 +1,15 @@
 import 'dart:convert';
 
-void main(List<String> arguments) {
-  var jsonString = '''
-  [
-    {"score": 40,
-    "name": "Alice"},
-    {"score": 80,
-    "name": "Bob"}
-  ]
-''';
-  var scores = jsonDecode(jsonString);
-  print(scores is List); // true
-  var firstScore = scores[0];
-  print(firstScore is Map); // true
-  print(firstScore['name']); // Alice
-  print(firstScore['score']); // 40
+var scores = [
+  {'score': 40},
+  {'score': 100},
+  {'score': 120, 'overtime': true, 'special_guest': null}
+];
+
+void main() {
+  var jsonText = jsonEncode(scores);
+  print(jsonText);
+
+  print(jsonText == "[{'score': 40},{'score': 100},{'score': 120, 'overtime': true, 'special_guest': null}]"); // false
+  print(jsonText == '[{"score":40},{"score":100},{"score":120,"overtime":true,"special_guest":null}]'); // true
 }
