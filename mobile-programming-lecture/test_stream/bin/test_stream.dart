54bc7176e6a12874354addfa05c6380d842deed9
soaringwave <82134672+grootwo@users.noreply.github.com>
2025-09-19 11:01:09 +0900

    feat: move first statement

--- mobile-programming-lecture/test_stream/bin/test_stream.dart
+++ mobile-programming-lecture/test_stream/bin/test_stream.dart
@@ -3,8 +3,8 @@ import 'dart:async';
 void main() async {
   var stream = Stream.fromIterable([1, 2, 3]);
 
-  stream.first.then((value) => print('first: $value'));
   stream.last.then((value) => print('last: $value'));
   stream.isEmpty.then((value) => print('isEmpty: $value'));
   stream.length.then((value) => print('length: $value'));
+  stream.first.then((value) => print('first: $value'));
 }
