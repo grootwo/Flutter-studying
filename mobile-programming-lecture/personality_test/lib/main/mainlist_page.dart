7cb5548638afe6e1c30ee317f4b9df7ed8ac59c7
soaringwave <82134672+grootwo@users.noreply.github.com>
2025-10-17 09:38:09 +0900

    feat: import remote_config and fetch

--- mobile-programming-lecture/personality_test/lib/main/mainlist_page.dart
+++ mobile-programming-lecture/personality_test/lib/main/mainlist_page.dart
@@ -5,6 +5,7 @@ import 'package:flutter/services.dart';
 import 'dart:convert';
 import '../sub/question_page.dart'; // [p.92] QuestionPage로 화면 이동을 위한 import
 import 'package:firebase_analytics/firebase_analytics.dart'; // [p.97] 클릭 이벤트 로그 전송용 Firebase Analytics
+import 'package:firebase_remote_config/firebase_remote_config.dart';
 
 // [p.83] StatefulWidget: 상태 변화가 발생하는 동적 화면 구성 시 사용
 // MainPage는 JSON을 로드한 뒤 화면이 업데이트되므로 StatefulWidget이 적합합니다.
@@ -18,6 +19,29 @@ class MainPage extends StatefulWidget {
 // [p.84] _MainPage 상태 클래스: 실제 UI와 로직이 구현되는 곳
 // build() 메서드와 FutureBuilder.
 class _MainPage extends State<MainPage> {
+  final FirebaseRemoteConfig remoteConfig = FirebaseRemoteConfig.instance;
+
+  String welcomeTitle = '';
+  bool bannerUse = false;
+  int itemHeight = 50;
+
+  @override
+  void initState() {
+    super.initState();
+    remoteConfigInit(); // p.102 (3)
+  }
+
+  // p.102 (3)
+  Future<void> remoteConfigInit() async {
+    await remoteConfig.fetch();
+    await remoteConfig.activate();
+    setState(() {
+      welcomeTitle = remoteConfig.getString('welcome');
+      bannerUse = remoteConfig.getBool('banner');
+      itemHeight = remoteConfig.getInt('item_height');
+    });
+  }
+
   // [p.84] 비동기 JSON 파일 로드 함수
   // assets 폴더(res/api/list.json)에 저장된 테스트 목록 데이터를 불러옵니다.
   // rootBundle은 Flutter의 내장 리소스 접근 도구입니다.
