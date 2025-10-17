1ae5370e3bc9aeaf96d3f2dea2b1fd771e24a4c0
soaringwave <82134672+grootwo@users.noreply.github.com>
2025-10-17 09:28:38 +0900

    doc: download firebase_remote_config

--- mobile-programming-lecture/personality_test/macos/Flutter/GeneratedPluginRegistrant.swift
+++ mobile-programming-lecture/personality_test/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -7,8 +7,10 @@ import Foundation
 
 import firebase_analytics
 import firebase_core
+import firebase_remote_config
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FirebaseAnalyticsPlugin.register(with: registry.registrar(forPlugin: "FirebaseAnalyticsPlugin"))
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
+  FirebaseRemoteConfigPlugin.register(with: registry.registrar(forPlugin: "FirebaseRemoteConfigPlugin"))
 }
