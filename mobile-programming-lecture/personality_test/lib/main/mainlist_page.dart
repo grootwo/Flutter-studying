72a48c5e4fb7fe8e125b7fb5b4ada875dd9dad96
soaringwave <82134672+grootwo@users.noreply.github.com>
2025-10-17 10:23:16 +0900

    feat: import database and push data

--- mobile-programming-lecture/personality_test/lib/main/mainlist_page.dart
+++ mobile-programming-lecture/personality_test/lib/main/mainlist_page.dart
@@ -6,6 +6,7 @@ import 'dart:convert';
 import '../sub/question_page.dart'; // [p.92] QuestionPage로 화면 이동을 위한 import
 import 'package:firebase_analytics/firebase_analytics.dart'; // [p.97] 클릭 이벤트 로그 전송용 Firebase Analytics
 import 'package:firebase_remote_config/firebase_remote_config.dart';
+import 'package:firebase_database/firebase_database.dart';
 
 // [p.83] StatefulWidget: 상태 변화가 발생하는 동적 화면 구성 시 사용
 // MainPage는 JSON을 로드한 뒤 화면이 업데이트되므로 StatefulWidget이 적합합니다.
@@ -117,6 +118,52 @@ class _MainPage extends State<MainPage> {
             default:
               return const Center(child: Text('No Data'));
           }
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: const Icon(Icons.add),
+        onPressed: () {
+          final FirebaseDatabase database = FirebaseDatabase.instance;
+          final DatabaseReference testRef = database.ref('test');
+
+          // 첫 번째 데이터
+          testRef.push().set("""
+          {
+            "title": "당신이 좋아하는 동물은?",
+            "question": "무인도에 도착한 당신이 마침 떠내려온 상자를 열어보니 보이는 물건은?",
+            "selects": ["생존 키트", "휴대폰", "텐트", "책 무인도에서 살아남기"],
+            "answer": [
+               "당신은 현실주의 동물은 안 키웁니다",
+               "당신은 늘 함께 있는 걸 좋아하는 강아지가 어울립니다",
+               "당신은 같은 공간을 공유하는 고양이가 어울립니다",
+               "당신은 낭만을 좋아하는 앵무새가 어울립니다"
+            ]
+          }
+          """);
+
+          // 두 번째 데이터
+          testRef.push().set('''
+          {
+            "title": "5초 MBTI I/E 판별",
+            "question": "친구와 함께 간 미술관 당신이라면?",
+            "selects": ["말이 많아짐", "생각이 많아짐"],
+            "answer": ["당신의 성향은 E", "당신의 성향은 I"]
+          }
+          ''');
+
+          // 세 번째 데이터
+          testRef.push().set("""
+          {
+            "title": "당신은 어떤 사랑을 하고 싶나요?",
+            "question": "목욕을 할 때 가장 먼저 비누칠을 하는 곳은?",
+            "selects": ["머리", "상체", "하체"],
+            "answer": [
+              "당신은 자만추를 추천해요",
+              "당신은 소개팅을 통한 새로운 사람의 소개를 좋아합니다",
+              "당신은 길가다가 우연히 지나친 그런 인연을 좋아합니다"
+            ]
+          }
+          """);
         },
       ),
     );
