bef3e0202ba87a8664788d360e5c5531360df9cb
soaringwave <82134672+grootwo@users.noreply.github.com>
2025-11-07 09:28:38 +0900

    feat: delete exception func

--- mobile-programming-lecture/my_budongsan/lib/main.dart
+++ mobile-programming-lecture/my_budongsan/lib/main.dart
@@ -37,12 +37,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: Scaffold(
-        appBar: AppBar(title: const Text("Crashlytics Test")),
-        body: TextButton(
-            onPressed: () => throw Exception('Throw Test Exception'), 
-            child: const Text('Throw Test Exception')),
-      ),
+      home: const IntroPage(),
     );
   }
 }
