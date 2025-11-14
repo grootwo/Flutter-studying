a4786197cd61a80db864f2390df0f9b37a8afc10
soaringwave <82134672+grootwo@users.noreply.github.com>
2025-11-14 13:46:11 +0900

    refactor

--- mobile-programming-lecture/my_budongsan/lib/myFavorite/my_favorite_page.dart
+++ mobile-programming-lecture/my_budongsan/lib/myFavorite/my_favorite_page.dart
@@ -1,7 +1,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:my_budongsan/map/apt_page.dart';
-import '../map/map_page.dart';
 
 class MyFavoritePage extends StatefulWidget {
   const MyFavoritePage({super.key});
@@ -18,35 +17,48 @@ class _MyFavoritePage extends State<MyFavoritePage> {
   @override
   void initState() {
     super.initState();
-    FirebaseFirestore.instance.collection("rollcake").doc("favorite").get().then((value) => {
-      setState(() {
-        favoriteList.add(value.data()!);
-      })
-    });
+    FirebaseFirestore.instance
+        .collection("rollcake")
+        .doc("favorite")
+        .get()
+        .then(
+          (value) => {
+            setState(() {
+              favoriteList.add(value.data()!);
+            }),
+          },
+        );
   }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text("나의 추천 리스트"),
-      ),
-      body: ListView.builder(itemBuilder: (context, snapshot) {
-        return Card(
-          child: InkWell(
-            child: SizedBox(
-              height: 50,
-              child: Column(
-                children: [Text(favoriteList[snapshot]["name"])],
+      appBar: AppBar(title: const Text("나의 추천 리스트")),
+      body: ListView.builder(
+        itemBuilder: (context, snapshot) {
+          return Card(
+            child: InkWell(
+              child: SizedBox(
+                height: 50,
+                child: Column(children: [Text(favoriteList[snapshot]["name"])]),
               ),
+              onTap: () {
+                Navigator.of(context).push(
+                  MaterialPageRoute(
+                    builder: (context) {
+                      return AptPage(
+                        aptHash: favoriteList[snapshot]["position"]["geohash"],
+                        aptInfo: favoriteList[snapshot],
+                      );
+                    },
+                  ),
+                );
+              },
             ),
-            onTap: () {
-              Navigator.of(context).push(MaterialPageRoute(builder: (context) {
-                return AptPage(aptHash: favoriteList[snapshot]["position"]["geohash"], aptInfo: favoriteList[snapshot]);
-              }));
-            },
-          ),
-        );
-      }),
+          );
+        },
+        itemCount: favoriteList.length,
+      ),
     );
   }
 }
