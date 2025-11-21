5393f482c6249e8fcd5f16dde2e2335d944d5682
soaringwave <82134672+grootwo@users.noreply.github.com>
2025-11-21 10:39:06 +0900

    design: chane colorScheme

--- mobile-programming-lecture/classic_sound/lib/main.dart
+++ mobile-programming-lecture/classic_sound/lib/main.dart
@@ -20,7 +20,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.white12),
         useMaterial3: true,
       ),
       home: IntroPage(),
