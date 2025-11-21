e7da417180ed851e16912ebdbb1e0f1f18657f04
soaringwave <82134672+grootwo@users.noreply.github.com>
2025-11-21 11:24:10 +0900

    feat: update with music database

--- mobile-programming-lecture/classic_sound/lib/main.dart
+++ mobile-programming-lecture/classic_sound/lib/main.dart
@@ -3,16 +3,22 @@ import 'package:classic_sound/firebase_options.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:classic_sound/view/intro/intro_page.dart';
+import 'package:sqflite/sqflite.dart';
+import 'package:classic_sound/data/local_database.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
-  runApp(MyApp());
+  final Database database = await MusicDatabase.initDatabase();
+  runApp(MyApp(database: database,));
+
 }
 
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  final Database database;
+  const MyApp({super.key, required this.database});
+
 
   // This widget is the root of your application.
   @override
@@ -23,7 +29,9 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.white12),
         useMaterial3: true,
       ),
-      home: IntroPage(),
+      // home: UploadPage,
+      //home: UploadPage(),
+      home: IntroPage(database: database,),
     );
   }
 }
