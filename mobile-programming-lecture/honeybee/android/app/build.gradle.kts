8435f672875f4aa468f2d00eb4c953156ff44de7
soaringwave <82134672+grootwo@users.noreply.github.com>
2025-11-28 11:09:52 +0900

    build: set compileoptions to prevent error

--- mobile-programming-lecture/honeybee/android/app/build.gradle.kts
+++ mobile-programming-lecture/honeybee/android/app/build.gradle.kts
@@ -14,8 +14,9 @@ android {
     ndkVersion = flutter.ndkVersion
 
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
+        isCoreLibraryDesugaringEnabled = true
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
 
     kotlinOptions {
