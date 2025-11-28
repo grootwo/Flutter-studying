b5ef649872ebd330a29aac4e91e681f7ed9c7b11
soaringwave <82134672+grootwo@users.noreply.github.com>
2025-11-28 11:17:52 +0900

    build: change version of java target

--- mobile-programming-lecture/honeybee/android/app/build.gradle.kts
+++ mobile-programming-lecture/honeybee/android/app/build.gradle.kts
@@ -20,15 +20,15 @@ android {
     }
 
     kotlinOptions {
-        jvmTarget = JavaVersion.VERSION_11.toString()
+        jvmTarget = "17"
     }
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.rollcake.honeybee"
+        applicationId = "com.example.honeybee"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = 23
+        minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
@@ -40,6 +40,10 @@ android {
             // Signing with the debug keys for now, so `flutter run --release` works.
             signingConfig = signingConfigs.getByName("debug")
         }
+    }
+
+    dependencies {
+        coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.1.4")
     }
 }
 
